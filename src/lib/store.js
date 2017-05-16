dc316b4659a15c23ab904ff0306734df7e1305f1
Adamki <adajensen@gmail.com>
2017-05-16 04:13:59 -0700

    adds middleware ability.

--- src/lib/store.js
+++ src/lib/store.js
@@ -1,23 +1,24 @@
 const validateAction = action => {
   if (!action || typeof action !== 'object' || Array.isArray(action)) {
-    throw new Error('Action must be an Object');
+    throw new Error('Action must be an object!');
   }
   if (typeof action.type === 'undefined') {
-    throw new Error('Action must have a Type');
+    throw new Error('Action must have a type!');
   }
 };
 
-export const createStore = reducer => {
+export const createStore = (reducer, middleware) => {
   let state;
   const subscribers = [];
+  const coreDispatch = action => {
+    validateAction(action);
+    state = reducer(state, action);
+    subscribers.forEach(handler => handler());
+  };
+  const getState = () => state;
   const store = {
-    dispatch: action => {
-      console.log(action)
-      validateAction(action);
-      state = reducer(state, action);
-      subscribers.forEach(handler => handler());
-    },
-    getState: () => state,
+    dispatch: coreDispatch,
+    getState,
     subscribe: handler => {
       subscribers.push(handler);
       return () => {
@@ -28,7 +29,13 @@ export const createStore = reducer => {
       };
     }
   };
-  store.dispatch({type: '@@redux/INIT'});
+  if (middleware) {
+    const dispatch = action => store.dispatch(action);
+    store.dispatch = middleware({
+      dispatch,
+      getState
+    })(coreDispatch);
+  }
+  coreDispatch({type: '@@redux/INIT'});
   return store;
 };
-
